800e95b3888210284bc4422f1622a5bb3f489a2c
vinyllemon24 <raquel.guerra2403@gmail.com>
2018-11-30 16:45:54 +0000

    fixeeee

--- Comprimidos/src/comprimidos/Comprimido.java
+++ Comprimidos/src/comprimidos/Comprimido.java
@@ -2,4 +2,8 @@ package comprimidos;
 
 public class Comprimido extends Medicamento {
     
+    public Comprimido(int id, String nome, String descricao, Data periodo_toma, int quantidade) {
+        super(id, nome, descricao, periodo_toma, quantidade);
+    }
+    
 }
