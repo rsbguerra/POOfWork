415573be812e7e141bfe2cb896f435f8f929e48e
UselessFuck <henriquejca1999@gmail.com>
2018-12-08 16:04:14 +0000

    Update ArrayVazio.java

--- Comprimidos/src/comprimidos/ArrayVazio.java
+++ Comprimidos/src/comprimidos/ArrayVazio.java
@@ -1,10 +1,12 @@
 package comprimidos;
-
-public class Comprimido extends Medicamento {
     
-    public Comprimido(int id, String nome, String descricao, Data periodo_toma, int quantidade) {
-        super(id, nome, descricao, periodo_toma, quantidade);
-        //windows e cancro
-    }
-    
+   public class ArrayVazio extends Exception{
+       
+       public ArrayVazio(){
+           super();
+   }
+       
+   public ArrayVazio(String s){
+       super(s);
+   }
 }
